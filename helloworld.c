691ed0bde98a4bd2d3a6a5d632cf92e88c4b623a
“ThiesF” <t.friesema@student.avans.nl>
2026-09-11 11:57:29 +0200

    My third commit

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-   printf("Hello, Galaxy, Universe and everything beyond!");
+   printf("Hello, Galaxy, Universe and everything beyond!");   //edditted by Thies
    return 0;
 }
